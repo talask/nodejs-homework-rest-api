307d6f8dca9c5e4f94443d2f38e13e95a8bc0778
talask <natasha169@ukr.net>
2023-08-19 11:35:07 +0300

    connect db

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const app = require("./app");
 
 const mongoose = require('mongoose');
+//const env = require('dot');
 
-
+const DB_HOST = "mongodb+srv://Natalya:aLQIpiovXG1LrT0o@cluster0.hiykldq.mongodb.net/connect_db?retryWrites=true&w=majority";
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(DB_HOST);
+// mongoose.connect(process.env.DB_HOST, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
 
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
