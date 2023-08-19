2eedd9115fcd25af639f4ea0bf868ead097b9d7d
talask <natasha169@ukr.net>
2023-08-19 11:20:41 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require("./app");
 
+const mongoose = require('mongoose');
+
+
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(DB_HOST);
+
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
 });
