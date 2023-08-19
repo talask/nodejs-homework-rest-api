f3f3abeb43134ae1781129373f15a5846a4ccd10
talask <natasha169@ukr.net>
2023-08-19 12:09:56 +0300

    fixed

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const mongoose = require('mongoose');
 //const env = require('dot');
 
 //const DB_HOST = "mongodb+srv://Natalya:aLQIpiovXG1LrT0o@cluster0.hiykldq.mongodb.net/connect_db?retryWrites=true&w=majority";
+
+require('dotenv').config();
+
+
 const { DB_HOST } = process.env;
-
 mongoose.Promise = global.Promise;
+console.log(DB_HOST)
+//mongoose.connect(DB_HOST);
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect(process,env.DB_HOST);
-// mongoose.connect(process.env.DB_HOST, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
 
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
