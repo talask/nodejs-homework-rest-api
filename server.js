eafad0f3922d694b40f82effe87971b5308a57dd
talask <natasha169@ukr.net>
2023-08-19 11:46:46 +0300

    fixed

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = require("./app");
 const mongoose = require('mongoose');
 //const env = require('dot');
 
-const DB_HOST = "mongodb+srv://Natalya:aLQIpiovXG1LrT0o@cluster0.hiykldq.mongodb.net/connect_db?retryWrites=true&w=majority";
+//const DB_HOST = "mongodb+srv://Natalya:aLQIpiovXG1LrT0o@cluster0.hiykldq.mongodb.net/connect_db?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(DB_HOST);
+mongoose.connect(process,env.DB_HOST);
 // mongoose.connect(process.env.DB_HOST, {
 //   useNewUrlParser: true,
 //   useCreateIndex: true,
